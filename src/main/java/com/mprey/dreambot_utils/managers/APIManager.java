84783e579f998c38143f3f0c7e5af5d40e561993
mprey <masonprey7@gmail.com>
2020-01-19 23:40:21 -0600

    update6

--- src/main/java/com/mprey/dreambot_utils/managers/APIManager.java
+++ src/main/java/com/mprey/dreambot_utils/managers/APIManager.java
@@ -1,12 +1,50 @@
 package com.mprey.dreambot_utils.managers;
 
+import com.google.gson.Gson;
 import com.mprey.dreambot_utils.DreambotUtils;
 import com.mprey.dreambot_utils.statics.Environment;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 public class APIManager {
 
-    public static Object sendRequest() {
-        return null;
+    public static <T> T sendGETRequest(String endpoint, Class<T> responseType) {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(getURLBase() + endpoint);
+
+        try {
+            CloseableHttpResponse resp = client.execute(httpGet);
+            String json = EntityUtils.toString(resp.getEntity());
+            return new Gson().fromJson(json, responseType);
+        } catch (Exception e) {
+            DreambotUtils.log("Unable to send GET request to " + endpoint);
+            DreambotUtils.log(e.getMessage());
+            return null;
+        }
+    }
+
+    public static <T> T sendPOSTRequest(String endpoint, Object body, Class<T> responseType) {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(getURLBase() + endpoint);
+        HttpEntity stringEntity = new StringEntity(new Gson().toJson(body), ContentType.APPLICATION_JSON);
+        httpPost.setEntity(stringEntity);
+
+        try {
+            CloseableHttpResponse resp = client.execute(httpPost);
+            String json = EntityUtils.toString(resp.getEntity());
+            return new Gson().fromJson(json, responseType);
+        } catch (Exception e) {
+            DreambotUtils.log("Unable to send POST request to " + endpoint);
+            DreambotUtils.log(e.getMessage());
+            return null;
+        }
     }
 
     private static String getURLBase() {
---
+++ src/main/java/com/mprey/dreambot_utils/managers/AccountManager.java
@@ -0,0 +1,25 @@
+package com.mprey.dreambot_utils.managers;
+
+import com.mprey.dreambot_utils.models.Account;
+
+public class AccountManager {
+
+    public static Account requestAccount() {
+        // TODO
+        return null;
+    }
+
+    public static Account replaceAccount(Account old) {
+        // TODO
+        return null;
+    }
+
+    public static void returnAccount(Account account) {
+        // TODO
+    }
+
+    public static void createAccount(Account account) {
+        // TODO
+    }
+
+}
