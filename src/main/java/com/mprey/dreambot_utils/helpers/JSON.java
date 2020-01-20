1963d65ffd9276cf20422c2ec2b99d54209275fd
mprey <masonprey7@gmail.com>
2020-01-19 21:49:03 -0600

    Update JSON

--- src/main/java/com/mprey/dreambot_utils/helpers/JSON.java
+++ src/main/java/com/mprey/dreambot_utils/helpers/JSON.java
@@ -1,14 +1,11 @@
 package com.mprey.dreambot_utils.helpers;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.util.*;
 
 public class JSON {
 
-    public static Map<String, Object> parseParams(String params) {
-        return new Gson().fromJson(params, new TypeToken<HashMap<String, Object>>() {}.getType());
+    public static <T> T parseParams(String params, Class<T> type) {
+        return new Gson().fromJson(params, type);
     }
 
 }
