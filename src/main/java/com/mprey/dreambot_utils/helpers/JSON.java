f465c3685aedd6c3cf74331ae2f9d60a905bf2f9
mprey <masonprey7@gmail.com>
2020-01-19 22:31:14 -0600

    update4

--- src/main/java/com/mprey/dreambot_utils/helpers/JSON.java
+++ src/main/java/com/mprey/dreambot_utils/helpers/JSON.java
@@ -1,23 +1,11 @@
 package com.mprey.dreambot_utils.helpers;
 
 import com.google.gson.Gson;
-import org.dreambot.api.script.AbstractScript;
 
 public class JSON {
 
     public static <T> T parseParams(String params, Class<T> type) {
-        AbstractScript.log("in the thick: " + params);
-        try {
-            AbstractScript.log(new Gson().toString());
-            AbstractScript.log("Test");
-            T result = new Gson().fromJson(params, type);
-            AbstractScript.log("GOT T " + result.toString());
-            return result;
-        } catch (Exception e) {
-            AbstractScript.log("ERRORRRR");
-        }
-        AbstractScript.log("BOTTOM");
-        return null;
+        return new Gson().fromJson(params, type);
     }
 
 }
