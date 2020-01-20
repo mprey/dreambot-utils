359b51d49701ebe2ebb54d76b0a552929c253f35
mprey <masonprey7@gmail.com>
2020-01-19 22:10:06 -0600

    update

--- src/main/java/com/mprey/dreambot_utils/helpers/JSON.java
+++ src/main/java/com/mprey/dreambot_utils/helpers/JSON.java
@@ -8,9 +8,11 @@ public class JSON {
     public static <T> T parseParams(String params, Class<T> type) {
         AbstractScript.log("in the thick: " + params);
         try {
-            return new Gson().fromJson(params, type);
+            T result = new Gson().fromJson(params, type);
+            AbstractScript.log("GOT T " + result.toString());
+            return result;
         } catch (Exception e) {
-            AbstractScript.log(e.getMessage());
+            AbstractScript.log("ERRORRRR"));
         }
         AbstractScript.log("BOTTOM");
         return null;
