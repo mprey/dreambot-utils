fdcd9f310f19bff27af494aa370fe170d410f869
mprey <masonprey7@gmail.com>
2020-03-19 21:54:23 -0500

    Add Ack

--- src/main/java/com/mprey/dreambot_utils/managers/SocketManager.java
+++ src/main/java/com/mprey/dreambot_utils/managers/SocketManager.java
@@ -2,6 +2,7 @@ package com.mprey.dreambot_utils.managers;
 
 import com.mprey.dreambot_utils.DreambotUtils;
 import com.mprey.dreambot_utils.statics.Endpoint;
+import io.socket.client.Ack;
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
@@ -31,7 +32,11 @@ public class SocketManager {
     public static void registerDefaultListeners() {
         socket.on("SHUTDOWN", (objects -> DreambotUtils.log("GOT SHUTDOWN" + objects[0])));
 
-        socket.on("GET_UUID", (objects -> DreambotUtils.getScript().getParams().getUUID()));
+        socket.on("GET_UUID", (objects -> {
+            DreambotUtils.log("Got request for UUID");
+            Ack ack = (Ack) objects[0];
+            ack.call(DreambotUtils.getScript().getParams().getUUID());
+        }));
     }
 
 }
